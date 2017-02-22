08a4c435b48713cc01493e89f65089b91d410a19
Dylan Jenkinson <dylan.jenkinson@xtra.co.nz>
2017-02-23 12:37:48 +1300

    Pk, something is funny with the arcade viewport stuff. Wrote own scrolling code. Works lovely. Use set_viewport, works wonky like. Should maybe make simple implementation to share with dev to see if he can point me in the right direction.

--- source/app.py
+++ source/app.py
@@ -8,8 +8,8 @@ from arcade import load_textures
 
 SPRITE_SCALING = 0.5
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 1200
 
 
 class MyApplication(arcade.Window):
@@ -20,21 +20,28 @@ class MyApplication(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.all_sprites_list = None
+        self.player_sprite = None
         self.sprite_sheet = None
         self.tile_set = None
-        self.viewport_bottom = 0.0
+        self.viewport_bottom = 128.0
 
     def load_layer(self, layer):
         for tile in layer.tiles():
             tile_sprite = arcade.Sprite()
             tile_texture_positions = tile[2][1]
-            x, y = tile_texture_positions[0] / 64, tile_texture_positions[1] / 64
+            x, y = int(tile_texture_positions[0] / 64), int(tile_texture_positions[1] / 64)
             offset = y * 16 + x
             tile_sprite.append_texture(self.tile_set[int(offset)])
-            print(tile[0], tile[1])
-            tile_sprite.set_position(center_x=32 + tile[0]* 64, center_y=32 + (70 - tile[1]) * 64)
+            tile_sprite.set_position(center_x=32 + tile[0] * 64, center_y=32 + (70 - tile[1]) * 64)
             tile_sprite.set_texture(0)
             self.all_sprites_list.append(tile_sprite)
+
+    def load_tiles(self):
+        tile_set = []
+        for idx in range(1, 97):
+            texture = arcade.load_texture('../assets/sprites/tiles/tile_{:0>2}.png'.format(idx))
+            tile_set.append(texture)
+        return tile_set
 
     def setup(self):
         self.all_sprites_list = arcade.SpriteList()
@@ -56,48 +63,72 @@ class MyApplication(arcade.Window):
         tile_coordinates = []
         for y in range(0, 6):
             for x in range(0, 16):
-                tile_coordinates.append([x * 64, y * 64, 64, 64])
+                tile_coordinates.append([int(x * 64), int(y * 64), 64, 64])
         self.tile_set = load_textures(
             file_name='../assets/sprites/tiles_sheet.png',
             image_location_list=tile_coordinates
         )
+        # self.tile_set = self.load_tiles()
         level = pytmx.TiledMap('../assets/levels/pim-test-3.tmx')
 
         self.load_layer(layer=level.get_layer_by_name('water'))
         self.load_layer(layer=level.get_layer_by_name('overlay'))
         self.load_layer(layer=level.get_layer_by_name('land'))
-        self.load_layer(layer=level.get_layer_by_name('props'))
+        # self.load_layer(layer=level.get_layer_by_name('props'))
 
-
+        for sprite in self.all_sprites_list:
+            x = sprite.position[0]
+            y = int(sprite.position[1] - self.viewport_bottom)
+            sprite.set_position(center_x=x, center_y=y)
+            sprite.update()
 
         # for layer in level.layers:
         #     for tile in layer
         # Then make a sprite, and then give it the texture
-        sprite = arcade.Sprite()
-        sprite.append_texture(self.sprite_sheet[85])
-        sprite.set_position(center_x=400, center_y=300)
-        sprite.set_texture(0)
-        sprite.angle = 180
-        self.all_sprites_list.append(sprite)
+        self.player_sprite = arcade.Sprite()
+        self.player_sprite.append_texture(self.sprite_sheet[85])
+        self.player_sprite.set_position(center_x=400, center_y=300)
+        self.player_sprite.set_texture(0)
+        self.player_sprite.angle = 180
+
         # Don't show the mouse cursor
         self.set_mouse_visible(False)
 
         # Set the background color
-        arcade.set_background_color(arcade.color.AMAZON)
+        arcade.set_background_color(arcade.color.AMARANTH)
+
+    def own_scrolling(self, viewport_delta):
+        for sprite in self.all_sprites_list:
+            x = sprite.position[0]
+            y = int(sprite.position[1] - viewport_delta)
+            sprite.set_position(center_x=x, center_y=y)
+            sprite.update()
 
     def animate(self, delta_time):
         """ Movement and game logic """
-        sprite = self.all_sprites_list[-1]
         delta_angle = 36.0 * delta_time
-        self.viewport_bottom += 64 * delta_time
-        if self.viewport_bottom > ((70 * 64) - 600):
-            self.viewport_bottom = ((70 * 64) - 600)
+        viewport_delta = 64 * delta_time
+        self.viewport_bottom = int(self.viewport_bottom + viewport_delta)
+        print(self.viewport_bottom, self.player_sprite.position[1])
 
-        if sprite.position[1] < self.viewport_bottom + 55:
-            sprite.set_position(center_x=sprite.position[0], center_y=self.viewport_bottom + 55)
-        arcade.set_viewport(left=0, right=800, bottom=int(self.viewport_bottom), top=int(self.viewport_bottom) + 600)
+        if self.viewport_bottom > ((70 * 64) - SCREEN_HEIGHT):
+            self.viewport_bottom = ((70 * 64) - SCREEN_HEIGHT)
+
+        # x = self.player_sprite.position[0]
+        # y = self.player_sprite.position[1] - viewport_delta
+        # self.player_sprite.set_position(center_x=x, center_y=y)
+        # if self.player_sprite.position[1] < self.viewport_bottom:
+        #     self.player_sprite.set_position(center_x=self.player_sprite.position[0], center_y=self.viewport_bottom)
+        # print(int(self.viewport_bottom), int(self.viewport_bottom) + SCREEN_HEIGHT)
+        # print(0, SCREEN_WIDTH)
+
+        self.own_scrolling(viewport_delta)
+        # arcade.set_viewport(left=0, right=SCREEN_WIDTH, bottom=int(self.viewport_bottom), top=int(self.viewport_bottom) + SCREEN_HEIGHT)
+        # arcade.set_viewport(left=0, right=800, bottom=int(self.viewport_bottom), top=int(self.viewport_bottom) + 600)
         # sprite.angle += delta_angle
-        sprite.update()
+        self.player_sprite.update()
+        # self.dispatch_event('on_close')
+        # arcade.pause(0.3)
 
     def on_draw(self):
         """
@@ -109,9 +140,18 @@ class MyApplication(arcade.Window):
 
         # Draw all the sprites.
         self.all_sprites_list.draw()
+        self.player_sprite.draw()
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.ESCAPE and not (modifiers & ~(arcade.key.MOD_NUMLOCK |
+                                                              arcade.key.MOD_CAPSLOCK |
+                                                              arcade.key.MOD_SCROLLLOCK)):
+            self.dispatch_event('on_close')
 
 
 window = MyApplication(SCREEN_WIDTH, SCREEN_HEIGHT)
+window.set_fullscreen()
+
 window.setup()
 
 arcade.run()
